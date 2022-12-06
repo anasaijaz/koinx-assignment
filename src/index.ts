f35c4b285f0013dbc3979612de72bdf693226d47
anas_aijaz <cjphonehomec@gmail.com>
2022-12-06 17:58:54 +0530

    'initial-setup-commit'

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import { config } from "dotenv";
+import express, {
+  Application,
+  ErrorRequestHandler,
+  NextFunction,
+  Request,
+  Response,
+} from "express";
+import { Server } from "http";
+import createError from "http-errors";
+
+config({ path: `.env.${process.env.NODE_ENV}` });
+
+const app: Application = express();
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.end("Hello from other side!");
+});
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(new createError.NotFound());
+});
+
+const errorHandler: ErrorRequestHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  res.status(err.status || 500).json({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
+};
+
+app.use(errorHandler);
+
+const PORT: Number = Number(process.env.PORT) || 5000;
+
+const server: Server = app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}...`);
+});
